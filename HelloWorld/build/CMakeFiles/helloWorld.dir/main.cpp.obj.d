ef83b911abffa997681730c1d7dbb8205199d771
Khadija Naji <khadeja.mn005@gmail.com>
2026-09-05 20:56:07 +0300

    first commit

---
+++ HelloWorld/build/CMakeFiles/helloWorld.dir/main.cpp.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/helloWorld.dir/main.cpp.obj: \
+ H:\LearnOpenGL\LearnOpenGL\HelloWorld\main.cpp \
+ H:/LearnOpenGL/LearnOpenGL/Libs/glad/include/glad/glad.h \
+ H:/LearnOpenGL/LearnOpenGL/Libs/glad/include/KHR/khrplatform.h \
+ C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/16.1.0/include/stdint.h \
+ C:/msys64/ucrt64/include/stdint.h C:/msys64/ucrt64/include/crtdefs.h \
+ C:/msys64/ucrt64/include/corecrt.h C:/msys64/ucrt64/include/_mingw.h \
+ C:/msys64/ucrt64/include/_mingw_mac.h \
+ C:/msys64/ucrt64/include/_mingw_secapi.h \
+ C:/msys64/ucrt64/include/vadefs.h \
+ C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/16.1.0/include/stddef.h \
+ C:/msys64/ucrt64/include/stddef.h \
+ H:/LearnOpenGL/LearnOpenGL/libs/glfw/include/GLFW/glfw3.h
